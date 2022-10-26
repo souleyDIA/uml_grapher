a2869e09e5208eca68941777401c4198bcc9b751
SouleyDIA <Souleymane.dia@edu.devinci.fr>
2022-10-26 19:43:26 +0200

    init constructors outside the function

--- src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
+++ src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
@@ -4,27 +4,26 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class UmlGraph {
- 
+
     private final Class<?> cls;
- 
     public UmlGraph(Class<?> cls) {
         this.cls = cls;
     }
+    RelationFormater formater = new RelationFormater(); 
+    MermaidHelper helper = new MermaidHelper();
+    FieldFormater fieldFormater = new FieldFormater(helper); 
+    MethodFormater methodFormater = new MethodFormater(new ParameterFormater());
+    InterfaceFormater interfaceFormater = new InterfaceFormater();  
+    ClassFormater classFormater = new ClassFormater(fieldFormater, methodFormater, interfaceFormater);   
 
     public String as(GraphType type) {
         if (type == GraphType.Mermaid) {
-            RelationFormater formater = new RelationFormater(); 
-            MermaidHelper helper = new MermaidHelper();
-            FieldFormater fieldFormater = new FieldFormater(helper); 
-            MethodFormater methodFormater = new MethodFormater(new ParameterFormater());
-            InterfaceFormater interfaceFormater = new InterfaceFormater();  
-            ClassFormater classFormater = new ClassFormater(fieldFormater, methodFormater, interfaceFormater);
             String classDiagram = Stream.of(cls.getDeclaredClasses())
-                .map(classFormater::format)
-                .collect(Collectors.joining("\n"));
+            .map(classFormater::format)
+            .collect(Collectors.joining("\n"));
+
             String classDiagramOfGivenClass = classFormater.format(cls);
             String relationDiagram = formater.format(cls);
-
             return  "classDiagram\n" + classDiagram + "\n" + classDiagramOfGivenClass + "\n" + relationDiagram + "\n"; 
         }
         return "";
